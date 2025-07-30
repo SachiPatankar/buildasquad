1bead0eb8f95e697f4030e268d14a0c70c7e4565
Sachi Patankar <sachipatankar19@gmail.com>
2025-07-30 11:18:59 +0530

    Socket origin

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const socket: Socket = io(import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000', {
+const socket: Socket = io(import.meta.env.VITE_BACKEND_URL, {
   withCredentials: true,
   autoConnect: true,
 });
