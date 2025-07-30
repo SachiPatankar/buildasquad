8acdf904de702a61648be6ee25c451a5ab4f56d3
Sachi Patankar <sachipatankar19@gmail.com>
2025-07-30 13:32:16 +0530

    Polling

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -3,6 +3,7 @@ import { io, Socket } from 'socket.io-client';
 const socket: Socket = io(import.meta.env.VITE_API_URL, {
   withCredentials: true,
   autoConnect: true,
+  transports: ['polling'] 
 });
 
 
