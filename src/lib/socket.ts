c6f1676e94b8965ca470c7dccaf6d5b53ad481dd
Sachi Patankar <sachipatankar19@gmail.com>
2025-07-30 11:36:36 +0530

    Socket websocket force

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -3,6 +3,8 @@ import { io, Socket } from 'socket.io-client';
 const socket: Socket = io(import.meta.env.VITE_BACKEND_URL, {
   withCredentials: true,
   autoConnect: true,
+  transports: ['websocket'] 
 });
+
 
 export default socket; 
